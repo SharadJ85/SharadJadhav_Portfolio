3879357112b53b544b71c29bd672b0d2d4da584b
Sharad Jadhav <20425740+SharadJ85@users.noreply.github.com>
2020-02-05 21:05:05 +0530

    Initial commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,30 +4,33 @@ export default function Contact() {
   return (
     <div className="bg-dark-cyan py-10">
       <div>
-        <div className="uppercase email text-5xl text-center email">
-          Email
-        </div>
-      <form className="email">
-        <input
-          name="name"
-          type="text" 
-          className="feedback-input "
-          placeholder="Name"
-        />
-        <input
-          name="email"
-          type="text"
-          className="feedback-input "
-          placeholder="Email"
-        />
-        <textarea
-          name="text"
-          className="feedback-input email"
-          placeholder="Message"
-        ></textarea>
-        <input type="submit" value="SEND" />
-      </form>
-    </div>
+        <div className="uppercase email text-5xl text-center email">Email</div>
+        <form
+          name="contact"
+          method="POST"
+          data-netlify="true"
+          className="email"
+        >
+          <input
+            name="name"
+            type="text"
+            className="feedback-input "
+            placeholder="Name"
+          />
+          <input
+            name="email"
+            type="email"
+            className="feedback-input "
+            placeholder="Email"
+          />
+          <textarea
+            name="message"
+            className="feedback-input email"
+            placeholder="Message"
+          ></textarea>
+          <input type="submit" value="SEND" />
+        </form>
+      </div>
     </div>
   );
 }
