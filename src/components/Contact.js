fd03703ca74c19003cb767a9ee71a021e40a402a
Sharad Jadhav <20425740+SharadJ85@users.noreply.github.com>
2020-02-05 22:44:56 +0530

    initial

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,42 +1,38 @@
 import React from "react";
 
 export default function Contact() {
-    return (
-      <div className="bg-dark-cyan py-10">
-        <div>
-          <div className="uppercase email text-5xl text-center email">Email</div>
-          <form
-            name="contactForm"
-            action="POST"
-            method="POST"
-            netlify
-            data-netlify="true"
-            className="email"
-          >
-            <input
-              name="name"
-              type="text"
-              className="feedback-input "
-              placeholder="Name" 
-            />
-            <input
-              name="email"
-              type="email"
-              className="feedback-input "
-              placeholder="Email"
-            />
+  return (
+    <div className="bg-dark-cyan py-10">
+      <div>
+        <div className="uppercase email text-5xl text-center email">Email</div>
+        <form
+          name="contactForm"
+          action="POST"
+          method="POST"
+          netlify
+          data-netlify="true"
+        >
+          <div className="feedback-input ">
+            <input name="name" type="text" placeholder="Name" />
+          </div>
+          <div className="feedback-input ">
+            <input name="email" type="email" placeholder="Email" />
+          </div>
+          <div className="feedback-input email">
             <textarea
               name="message"
               className="feedback-input email"
               placeholder="Message"
             ></textarea>
-            <div class="field">
-            <div 
-            data-netlify-recaptcha="true"></div>
-            </div>
-            <button type="submit">SEND</button>
-          </form>
-        </div>
+          </div>
+          <div class="field">
+            <div data-netlify-recaptcha="true"></div>
+          </div>
+          <div>
+            <input type="submit" value="SEND" />
+          </div>
+        </form>
       </div>
-    );
-  };
+    </div>
+  );
+}
