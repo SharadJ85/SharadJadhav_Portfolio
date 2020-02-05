193e965800c9d2e06367710ca29761a89242625e
Sharad Jadhav <20425740+SharadJ85@users.noreply.github.com>
2020-02-05 21:40:35 +0530

    initial commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,37 +1,38 @@
 import React from "react";
 
 export default function Contact() {
-  return (
-    <div className="bg-dark-cyan py-10">
-      <div>
-        <div className="uppercase email text-5xl text-center email">Email</div>
-        <form
-          name="contact"
-          method="POST"
-          data-netlify="true"
-          data-netlify-recaptcha="true"
-          className="email"
-        >
-          <input
-            name="name"
-            type="text"
-            className="feedback-input "
-            placeholder="Name"
-          />
-          <input
-            name="email"
-            type="email"
-            className="feedback-input "
-            placeholder="Email"
-          />
-          <textarea
-            name="message"
-            className="feedback-input email"
-            placeholder="Message"
-          ></textarea>
-          <input type="submit" value="SEND" />
-        </form>
+  let msg=(()=>{alert("Thank you. I will get back to you as soon as possible!")})
+    return (
+      <div className="bg-dark-cyan py-10">
+        <div>
+          <div className="uppercase email text-5xl text-center email">Email</div>
+          <form
+            name="contact"
+            method="POST"
+            data-netlify="true"
+            data-netlify-recaptcha="true"
+            className="email"
+          >
+            <input
+              name="name"
+              type="text"
+              className="feedback-input "
+              placeholder="Name" 
+            />
+            <input
+              name="email"
+              type="email"
+              className="feedback-input "
+              placeholder="Email"
+            />
+            <textarea
+              name="message"
+              className="feedback-input email"
+              placeholder="Message"
+            ></textarea>
+            <button type="submit" onClick={msg}>SEND</button>
+          </form>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  };
